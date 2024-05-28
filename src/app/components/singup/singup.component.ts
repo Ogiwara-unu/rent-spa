3a2ae34d25632ca9adc27f878879fdd122e7f2e5
Randall_Wa <randall.alvarez.ch@gmail.com>
2024-05-27 23:39:35 -0600

    new signup desing

--- src/app/components/singup/singup.component.ts
+++ src/app/components/singup/singup.component.ts
@@ -1,37 +1,37 @@
 import { Component } from '@angular/core';
-import { User } from '../../models/user';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import Swal from 'sweetalert2';
-import { timer } from 'rxjs';
-
 
 @Component({
   selector: 'app-singup',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './singup.component.html',
-  styleUrl: './singup.component.css',
-  providers:[UserService]
+  styleUrls: ['./singup.component.css'],
+  providers: [UserService]
 })
 export class SingupComponent {
-  public status:number;
-  public user:User;
+  public status: number;
+  public user: User;
 
   constructor(
-    private _userService:UserService,
-    private _router:Router,
-    private _routes:ActivatedRoute){
-      this.status=-1; 
-      this.user=new User(1,"","","","");
+    private _userService: UserService,
+    private _router: Router,
+    private _routes: ActivatedRoute
+  ) {
+    this.status = -1;
+    this.user = new User(1, "", "", "", "");
   }
 
   onSubmit(form: any) {
     console.log("Registrando Usuario :v");
     this.user.rol = 'usuario';
     this._userService.create(this.user).subscribe({
-      next: (response => {
+      next: (response) => {
         if (response.status == 201) {
           form.reset();
           this.showAlert('success', response.message);
@@ -40,14 +40,13 @@ export class SingupComponent {
         } else {
           this.showAlert('error', response.message);
         }
-      }),
+      },
       error: (error: Error) => {
         this.showAlert('error', 'Error del servidor');
       }
     });
   }
-  
-  
+
   showAlert(type: 'success' | 'error', message: string) {
     Swal.fire({
       title: message,
@@ -56,6 +55,4 @@ export class SingupComponent {
       showConfirmButton: false
     });
   }
-  
- 
 }
