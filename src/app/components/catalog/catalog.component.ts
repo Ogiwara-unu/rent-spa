3bfcfd74272707e7c9a8f0a076869e367462e533
Randall_Wa <randall.alvarez.ch@gmail.com>
2024-05-30 15:22:52 -0600

    catalog attempt and update of service shi

---
+++ src/app/components/catalog/catalog.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { Vehiculo } from '../../models/vehiculo';
+import { VehiculoService } from '../../services/vehiculo.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-catalog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './catalog.component.html',
+  styleUrl: './catalog.component.css',
+  providers:[VehiculoService]
+})
+export class CatalogComponent {
+  public status:number;
+  public vehiculo:Vehiculo;
+  public vehiculos: Vehiculo[];
+
+  constructor(
+    private _vehiculoService:VehiculoService,
+    private _router:Router,
+    private _routes:ActivatedRoute){
+      this.status=-1;
+      this.vehiculo=new Vehiculo(1, "", "", "", "", 0, 0, "", "", "");
+      this.vehiculos = [];
+    }
+
+    ngOnInit(): void {
+      this.getVehiculos();
+    }
+
+    getVehiculos() {
+      this._vehiculoService.getCars().subscribe(
+        response => {
+          if (response.status === 200) {
+            this.vehiculos = response.data;
+            this.status = response.status;
+          } else {
+            this.status = response.status;
+          }
+        },
+        error => {
+          console.error(error);
+          this.status = error.status;
+        }
+      );
+    }
+
+
+}
+
+
