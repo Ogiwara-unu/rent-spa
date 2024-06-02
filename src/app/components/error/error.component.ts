6577bb921b6b1877e05e3b42da47a0fafae01fba
Randall_Wa <randall.alvarez.ch@gmail.com>
2024-06-02 00:00:04 -0600

    Update: Error page

--- src/app/components/error/error.component.ts
+++ src/app/components/error/error.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-error',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './error.component.html',
   styleUrl: './error.component.css'
 })
